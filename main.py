9cbdbca8ce46b134749dbbdb39dbb752404faba4
Antoine <32277387+Amanite2012@users.noreply.github.com>
2021-07-22 09:58:27 +0000

    Some documentation

--- main.py
+++ main.py
@@ -12,16 +12,22 @@ async def on_ready():
     print('My Ready is Body')
 @client.event
 async def on_message(message):
-    # Create voice channel
+    # Create voice channel for user
     if str(message.channel) == "spam" and message.content == "!create":
         if (message.author.VoiceState.channel == None):
             channel = client.get_channel()
             await channel.send("Vous devez etre connecte en vocal")
             
             
-        
+    # ----------------------------------
+    # Modify a message and anonymyze it
+    #
+    # To avoid the spam and saying stupid 
+    # things there is a logging possibility
+    # ----------------------------------
     elif str(message.channel) == "spam" and message.content != "":
         channel = client.get_channel(os.environ['CHANNEL_SENDING'])
+        # Logging the message sent
         channeladmin = client.get_channel(os.environ['CHANNEL_ADMIN'])
         radiomsg = rf.cricri(rf.obscure(message.content))
         await message.channel.purge(limit=1)
